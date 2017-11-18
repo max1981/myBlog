625239ff5c1b8046a1e8b641c701717fdb93898e
max <max_81@163.com>
2017-11-19 01:54:00 +0800

    改数据库密码

--- myBlog/settings.py
+++ myBlog/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'pagination_bootstrap.middleware.PaginationMiddleware',
+    # 'pagination_bootstrap.middleware.PaginationMiddleware',
 ]
 
 ROOT_URLCONF = 'myBlog.urls'
