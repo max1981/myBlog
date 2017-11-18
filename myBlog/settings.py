7364fe0cd71051cc92242705bd84a52bd9a150ee
max <max_81@163.com>
2017-11-19 00:29:53 +0800

    改数据库密码

--- myBlog/settings.py
+++ myBlog/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'myblog',
         'USER': 'root',
-        'PASSWORD': 'netease',
+        'PASSWORD': 'admin',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
