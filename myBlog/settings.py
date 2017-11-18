db25d70c20f1e4d87fe594d96d2f3307a9f200cc
max <max_81@163.com>
2017-11-19 01:32:23 +0800

    改数据库密码

--- myBlog/settings.py
+++ myBlog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0p!-4_t#fu-v63)5wyjawf5w-_zmo)6zyn(6q@di-hbiw&2-3*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
