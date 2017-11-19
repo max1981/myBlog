ca7b35d631f81361037182c301678d7e541be10e
max <max_81@163.com>
2017-11-20 00:33:00 +0800

    改数据库密码

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ import sys
 import os
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-execute(["scrapy", "crawl", "jobbole"])
+execute(["python", "manage.py", "runnserver",'0.0.0.0:9998'])
